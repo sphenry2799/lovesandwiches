79da4d0e5eda454f892faebe0520418bf8d8a66a
sphenry2799 <sp.henry2799@gmail.com>
2025-10-23 17:36:02 +0100

    Adjust validation to check for errors

--- run.py
+++ run.py
@@ -31,8 +31,9 @@ def validate_data(values):
     Raises valueError if string cannot be converted into int
     of if there are not exactly six values.
     """
-
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
